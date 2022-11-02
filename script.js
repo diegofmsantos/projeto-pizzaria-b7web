6fa6d5b07e187eaca8594c818be5694db7b0fcb4
Diego Santos <diegofmsantos0@gmail.com>
2022-11-01 22:34:39 -0300

    Até a aula 10

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+let cart = []
 let modalQt = 1
+let modalKey = 0
 
 const c = (el) => document.querySelector(el)
 const cs = (el) => document.querySelectorAll(el)
 
+// LISTAGEM DA PIZZAS
 pizzaJson.map((item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true)
     // preenche as informações em pizzaitem
@@ -16,6 +19,7 @@ pizzaJson.map((item, index) => {
         e.preventDefault()
         let key = e.target.closest('.pizza-item').getAttribute('data-key')
         modalQt = 1
+        modalKey = key
 
         c('.pizzaBig img').src = pizzaJson[key].img
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
@@ -31,13 +35,58 @@ pizzaJson.map((item, index) => {
 
         c('.pizzaInfo--qt').innerHTML = modalQt
 
-        c('.pizzaWindowArea').style.opacity = '0'
+        c('.pizzaWindowArea').style.opacity = 0
         c('.pizzaWindowArea').style.display = 'flex'
         setTimeout(() => {
-            c('.pizzaWindowArea').style.opacity = '1'
+            c('.pizzaWindowArea').style.opacity = 1
         }, 200)
 
     })
 
     c('.pizza-area').append(pizzaItem)
+})
+
+// EVENTOS DO MODAL
+
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0
+    setTimeout(() => {
+        c('.pizzaWindowArea').style.display = 'none'
+    }, 500)
+}
+
+cs('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((item) => {
+    item.addEventListener('click', closeModal)
+})
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if(modalQt > 1) {
+    modalQt--
+    c('.pizzaInfo--qt').innerHTML = modalQt
+    }
+})
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQt++
+    c('.pizzaInfo--qt').innerHTML = modalQt
+})
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=> {
+    size.addEventListener('click', () => {
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+        size.classList.add('selected')
+    })
+})
+
+c('.pizzaInfo--addButton').addEventListener('click', () => {
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'))
+    
+    cart.push({
+        id:pizzaJson[modalKey].id,
+        size:size,
+        qt:modalQt
+    })
+
+    closeModal()
+
 })
